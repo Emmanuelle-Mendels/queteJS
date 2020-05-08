917969514a1e37253ed07fe2e86f5641b7c01e4e
Melusine45 <emmanuelle.mendels@gmail.com>
2020-05-08 16:06:20 +0200

    first commit

---
+++ hello-wilder.js
@@ -0,0 +1,10 @@
+/* hello-wilder.js */
+
+// Define some variables
+const myFavoriteMovie = 'The Rocky Horror Picture Show';
+const director = 'Jim Sharman';
+const year = 1975;
+const message = myFavoriteMovie + ' directed by ' + director + ' in ' + year;
+
+// Display a popup
+alert(message);
